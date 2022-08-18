8f4e51486f03bc26eba4e67815362dd058fcd1ce
Adriano <adrianoivandepaulacampos@gmail.com>
2022-08-18 13:38:56 -0300

    resolução de conflitos, exibição de datas final e inicial de projetos no modal de escolha

--- frontend/src/pages/form-tarefas/chooseProjectModal/chooseProjectModal.component.tsx
+++ frontend/src/pages/form-tarefas/chooseProjectModal/chooseProjectModal.component.tsx
@@ -10,231 +10,248 @@ import style from "./chooseProjectModal.module.scss";
 import RegionPaginationButtons from "../../../components/region-pagination-buttons/region_pagination_buttons.component";
 import NegativeButtonModal from "../../../components/negative-button-modal/negative_button_modal.component";
 
-interface ChooseProjectModalProps{
-    setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
-    isOpen:boolean;
-    projectIdIfItIsToEdit:number |null;
-    equalizeAuxSelectedProjectToIdsProject: () => void;
-    setIdPRojectToChoose: React.Dispatch<SetStateAction<number | null>>;
-    setNameOfProjectToShow: React.Dispatch<SetStateAction<string | null>>;
+interface ChooseProjectModalProps {
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isOpen: boolean;
+  projectIdIfItIsToEdit: number | null;
+  equalizeAuxSelectedProjectToIdsProject: () => void;
+  setIdPRojectToChoose: React.Dispatch<SetStateAction<number | null>>;
+  setNameOfProjectToShow: React.Dispatch<SetStateAction<string | null>>;
 }
 
 const ChooseProjectModal = ({
-    setIsModalOpen,
-    isOpen,
-    projectIdIfItIsToEdit,
-    equalizeAuxSelectedProjectToIdsProject,
-    setNameOfProjectToShow,
-    setIdPRojectToChoose,
-  }:ChooseProjectModalProps) =>{
-    const {getProjectsByTitle } = useContext(ProjectContext);
-    const [projectToShow, setProjectToShow] = useState<Project[]>([]);
-    const [isLoaded, setIsLoaded] = useState(SpinnerState.Pending);
-    const [searchString, setSearchString] = useState("");
-    const [searchStringAux, setSearchStringAux] = useState("");
-    const [page, setPage] = useState(0);
-    const [size, setSize] = useState(6);
-    const [isLast, setIsLast] = useState(false);
-    const [isFirst, setIsFirst] = useState(false);
-    const [requestWasDone, setRequestWasDone] = useState(false);
-    const [auxIdProjectChoose, setAuxIdProjectChoose] = useState<number|null>(Number);
-    const [AuxnameOfProjectToShow, setAuxNameOfProjectToShow] = useState<string | null>("");
+  setIsModalOpen,
+  isOpen,
+  projectIdIfItIsToEdit,
+  equalizeAuxSelectedProjectToIdsProject,
+  setNameOfProjectToShow,
+  setIdPRojectToChoose,
+}: ChooseProjectModalProps) => {
+  const { getProjectsByTitle } = useContext(ProjectContext);
+  const [projectToShow, setProjectToShow] = useState<Project[]>([]);
+  const [isLoaded, setIsLoaded] = useState(SpinnerState.Pending);
+  const [searchString, setSearchString] = useState("");
+  const [searchStringAux, setSearchStringAux] = useState("");
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(6);
+  const [isLast, setIsLast] = useState(false);
+  const [isFirst, setIsFirst] = useState(false);
+  const [requestWasDone, setRequestWasDone] = useState(false);
+  const [auxIdProjectChoose, setAuxIdProjectChoose] = useState<number | null>(
+    Number
+  );
+  const [AuxnameOfProjectToShow, setAuxNameOfProjectToShow] = useState<
+    string | null
+  >("");
 
-    useEffect(() => {
-      if (isOpen) {
-        getProjectsByTitle(
-          searchString,
-          page,
-          size
-        ).then((response: any) => {
-          if (response?.last) {
-            setIsLast(true);
-          } else {
-            setIsLast(false);
-          }
-  
-          if (response?.first) {
-            setIsFirst(true);
-          } else {
-            setIsFirst(false);
-          }
-          if(projectIdIfItIsToEdit !== null){
-            setAuxIdProjectChoose(projectIdIfItIsToEdit);
-          }
-          setRequestWasDone(true);
-          if (response?.content) {
-            setProjectToShow(response.content);
-            console.log(response);
-          }
-        });
-      }
-    }, [isOpen, page, size, searchString]);
+  useEffect(() => {
+    if (isOpen) {
+      getProjectsByTitle(searchString, page, size).then((response: any) => {
+        if (response?.last) {
+          setIsLast(true);
+        } else {
+          setIsLast(false);
+        }
 
-    useEffect(() => {
-      if (projectToShow.length > 0) {
-        setIsLoaded(SpinnerState.Finished);
-      } else if (requestWasDone) {
-        setIsLoaded(SpinnerState.There_is_no_content);
-      }
-    }, [projectToShow]);
-  
+        if (response?.first) {
+          setIsFirst(true);
+        } else {
+          setIsFirst(false);
+        }
+        if (projectIdIfItIsToEdit !== null) {
+          setAuxIdProjectChoose(projectIdIfItIsToEdit);
+        }
+        setRequestWasDone(true);
+        if (response?.content) {
+          setProjectToShow(response.content);
+          console.log(response);
+        }
+      });
+    }
+  }, [isOpen, page, size, searchString]);
 
-    const decrementPage = () => {
-      if (page > 0) {
-        setPage(page - 1);
-      }
-    };
-  
-    const incrementPage = () => {
-      setPage(page + 1);
-    };
-  
-    const returnSpinner = () => {
-      return <AdagioSpinner loadingState={isLoaded} />;
-    };
+  useEffect(() => {
+    if (projectToShow.length > 0) {
+      setIsLoaded(SpinnerState.Finished);
+    } else if (requestWasDone) {
+      setIsLoaded(SpinnerState.There_is_no_content);
+    }
+  }, [projectToShow]);
 
-    return (
-      <Modal show={isOpen}>
-        <ModalHeader
-          message={"Escolha um Projeto"}
-          resetStateOfAuxState={equalizeAuxSelectedProjectToIdsProject}
-          setModalIsOpen={setIsModalOpen}
-        />
-  
-        <Modal.Body className={`${commonStyles.body_modal}`}>
-          <section>
-            <Form.Control
-              type="search"
-              onInput={(e: any) => {
-                setSearchStringAux(e.target.value);
-                console.log(e.code);
-                console.log("teste INPUT", searchStringAux);
-  
-                if (e.target.value.trim() === "") {
-                  setSearchString(e.target.value);
-                }
-              }}
-              onKeyDown={(e: any) => {
-                if (e.code.toLowerCase().trim() === "enter") {
-                  setSearchString(searchStringAux);
-                }
-              }}
-              title={"Pesquise por um Projeto, teclando ENTER "}
-              placeholder="Pesquise por uma Projeto, teclando ENTER "
-            ></Form.Control>
-          </section>
-          {isLoaded === SpinnerState.Finished && (
-            <>
-              <section>
+  const decrementPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const incrementPage = () => {
+    setPage(page + 1);
+  };
+
+  const extractDateTimeFromProject = (dateTime: string) => {
+    const dateTimeObject = new Date(dateTime);
+    console.log(dateTimeObject);
+    return `${dateTimeObject.getDate()}/${dateTimeObject.getMonth()}/${dateTimeObject.getFullYear()} ${dateTimeObject.getHours()}:${dateTimeObject.getMinutes()}`;
+  };
+  const returnSpinner = () => {
+    return <AdagioSpinner loadingState={isLoaded} />;
+  };
+
+  return (
+    <Modal show={isOpen}>
+      <ModalHeader
+        message={"Escolha um Projeto"}
+        resetStateOfAuxState={equalizeAuxSelectedProjectToIdsProject}
+        setModalIsOpen={setIsModalOpen}
+      />
+
+      <Modal.Body className={`${commonStyles.body_modal}`}>
+        <section>
+          <Form.Control
+            type="search"
+            onInput={(e: any) => {
+              setSearchStringAux(e.target.value);
+              console.log(e.code);
+              console.log("teste INPUT", searchStringAux);
+
+              if (e.target.value.trim() === "") {
+                setSearchString(e.target.value);
+              }
+            }}
+            onKeyDown={(e: any) => {
+              if (e.code.toLowerCase().trim() === "enter") {
+                setSearchString(searchStringAux);
+              }
+            }}
+            title={"Pesquise por um Projeto, teclando ENTER "}
+            placeholder="Pesquise por uma Projeto, teclando ENTER "
+          ></Form.Control>
+        </section>
+        {isLoaded === SpinnerState.Finished && (
+          <>
+            <section>
               <Form.Group
                 style={{
                   display: "flex",
                   alignItems: "center",
                   marginBottom: "0",
                 }}
-                  className="mb-1"
-                >
-              <Form.Control
-                type="radio"
-                name="choose"
-                id= "inputforNullProject"
-                style={{
-                  width: "22px",
-                  height: "18px",
-                }}
-                onClick={(e: any) => {
-                  setAuxIdProjectChoose(null);
-                }}
+                className="mb-1"
+              >
+                <Form.Control
+                  type="radio"
+                  name="choose"
+                  id="inputforNullProject"
+                  style={{
+                    width: "22px",
+                    height: "18px",
+                  }}
+                  onClick={(e: any) => {
+                    setAuxIdProjectChoose(null);
+                  }}
                 />
                 <Form.Label
-                   htmlFor={`inputforNullProject`}
-                   style={{
-                      display: "flex",
-                      marginTop: "8px",
-                      marginLeft: "8px",
-                      alignItems: "center",
+                  htmlFor={`inputforNullProject`}
+                  style={{
+                    display: "flex",
+                    marginTop: "8px",
+                    marginLeft: "8px",
+                    alignItems: "center",
                   }}
-                >Nenhum</Form.Label>
+                >
+                  Nenhum
+                </Form.Label>
               </Form.Group>
-                {projectToShow.map((project: Project) => {
-                  return (
-                    <div key={project.id + project.title}>
-                      <Form.Group
+              {projectToShow.map((project: Project) => {
+                const dateTimeStartToShow = extractDateTimeFromProject(
+                  project.dateTimeStart
+                );
+                const dateTimeEndToShow = extractDateTimeFromProject(
+                  project.dateTimeEnd
+                );
+                return (
+                  <div key={project.id + project.title}>
+                    <Form.Group
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "0",
+                      }}
+                      className="mb-1"
+                    >
+                      <Form.Control
+                        value={project.id}
+                        id={`project-${project.id}`}
+                        type="radio"
+                        name="choose"
+                        checked={
+                          project.id === auxIdProjectChoose ? true : false
+                        }
+                        style={{
+                          width: "22px",
+                          height: "18px",
+                        }}
+                        onClick={(e: any) => {
+                          if (project.id !== undefined)
+                            setAuxIdProjectChoose(project.id);
+                          setAuxNameOfProjectToShow(project.title);
+                        }}
+                      />
+                      <Form.Label
+                        htmlFor={`project-${project.id}`}
                         style={{
                           display: "flex",
+                          marginTop: "8px",
+                          marginLeft: "8px",
                           alignItems: "center",
-                          marginBottom: "0",
                         }}
-                        className="mb-1"
                       >
-                        <Form.Control
-                          value={project.id}
-                          id={`project-${project.id}`}
-                          type="radio"
-                          name="choose"
-                          checked={project.id === auxIdProjectChoose ? true:false}
-                          style={{
-                            width: "22px",
-                            height: "18px",
-                          }}
-                          onClick={(e: any) => {
-                            if(project.id !== undefined)
-                              setAuxIdProjectChoose(project.id);
-                              setAuxNameOfProjectToShow(project.title);
-                          }}
-                        />
-                        <Form.Label
-                          htmlFor={`project-${project.id}`}
-                          style={{
-                            display: "flex",
-                            marginTop: "8px",
-                            marginLeft: "8px",
-                            alignItems: "center",
-                          }}
-                        >
-                          {project.title}
-                        </Form.Label>
-                      </Form.Group>
-                    </div>
-                  );
-                })}
-              </section>
-            </>
-          )}
-  
-          <RegionPaginationButtons
-            isFirst={isFirst}
-            isLast={isLast}
-            decrementFunction={decrementPage}
-            incrementFunction={incrementPage}
-          />
-          {returnSpinner()}
-        </Modal.Body>
-  
-        <Modal.Footer>
-          <div className={commonStyles.confirmation_modal_buttons_container}>
-            <NegativeButtonModal
-              setModalIsOpen={setIsModalOpen}
-              resetStateOfAuxState={equalizeAuxSelectedProjectToIdsProject}
-              text={"Cancelar"}
-            />
-            <Button
-              tabIndex={1}
-              style={{ marginLeft: "25px" }}
-              onClick={() => {
-                setIsModalOpen(false);
-                setIdPRojectToChoose(auxIdProjectChoose);
-                setNameOfProjectToShow(AuxnameOfProjectToShow);
-              }}
-              variant="success"
-            >
-              {"Salvar"}
-            </Button>
-          </div>
-        </Modal.Footer>
-      </Modal>
-    );
+                        {project.title + " "}
+                        <span style={{ color: "blue", paddingLeft: "8px" }}>
+                          {" "}
+                          {` (${dateTimeStartToShow} - ${dateTimeEndToShow})`}
+                        </span>
+                      </Form.Label>
+                    </Form.Group>
+                  </div>
+                );
+              })}
+            </section>
+          </>
+        )}
 
-}
+        <RegionPaginationButtons
+          isFirst={isFirst}
+          isLast={isLast}
+          decrementFunction={decrementPage}
+          incrementFunction={incrementPage}
+        />
+        {returnSpinner()}
+      </Modal.Body>
+
+      <Modal.Footer>
+        <div className={commonStyles.confirmation_modal_buttons_container}>
+          <NegativeButtonModal
+            setModalIsOpen={setIsModalOpen}
+            resetStateOfAuxState={equalizeAuxSelectedProjectToIdsProject}
+            text={"Cancelar"}
+          />
+          <Button
+            tabIndex={1}
+            style={{ marginLeft: "25px" }}
+            onClick={() => {
+              setIsModalOpen(false);
+              setIdPRojectToChoose(auxIdProjectChoose);
+              setNameOfProjectToShow(AuxnameOfProjectToShow);
+            }}
+            variant="success"
+          >
+            {"Salvar"}
+          </Button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 export default ChooseProjectModal;
