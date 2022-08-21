a081ab37590022939081eec7f57e5d678d5b3f24
Adriano <adrianoivandepaulacampos@gmail.com>
2022-08-21 11:45:11 -0300

    mudança de string do endpoint

--- frontend/src/hooks/relatoryApi.ts
+++ frontend/src/hooks/relatoryApi.ts
@@ -4,7 +4,7 @@ const generatedIn = () => {
   const now = new Date();
   return `${String(now.getDate()).padStart(2, "0")}/${String(
     now.getMonth() + 1
-  ).padStart(2, "0")}/${String(now.getFullYear()).padStart(2, "0")} ${String(
+  ).padStart(2, "0")}/${String(now.getFullYear()).padStart(2, "0")}${String(
     now.getHours()
   ).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}:${String(
     now.getSeconds()
